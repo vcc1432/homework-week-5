c6f51322c1b15bb808ecfb6a2526c5ef66854f7d
vcc1432 <valerie.gersen@gmail.com>
2018-09-07 23:54:46 +0200

    Basic form container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import PizzaFormContainer from './components/PizzaFormContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Pizza Configurator!</h1>
+        <PizzaFormContainer />
       </div>
     );
   }
--- src/reducers/pizzas.js
+++ src/reducers/pizzas.js
@@ -1,4 +1,19 @@
-const reducer = (state = "test", action = {}) => {
+const initialState = {
+  base: {
+    size: '',
+    price: 0
+  },
+  sauce: {
+    name: '',
+    price: 0
+  },
+  toppings: [],
+  turboDrone: false
+}
+
+
+
+const reducer = (state = initialState, action = {}) => {
     switch (action.type) {
     default:
       return state
